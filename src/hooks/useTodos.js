440af1c37c7f0e821661fe58cd49c683fec7a820
Ben <benjaminfkile@gmail.com>
2026-04-25 19:55:52 -0500

    feat: complete task #234 - Implement todo state with add/toggle/delete

---
+++ src/hooks/useTodos.js
@@ -0,0 +1,36 @@
+import { useCallback } from 'react';
+import useLocalStorage from './useLocalStorage';
+import { createTodo } from '../todos/createTodo';
+
+const STORAGE_KEY = 'grunt-todos';
+
+export default function useTodos() {
+  const [todos, setTodos] = useLocalStorage(STORAGE_KEY, []);
+
+  const addTodo = useCallback(
+    (text) => {
+      setTodos((current) => [...current, createTodo(text)]);
+    },
+    [setTodos],
+  );
+
+  const toggleTodo = useCallback(
+    (id) => {
+      setTodos((current) =>
+        current.map((todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+        ),
+      );
+    },
+    [setTodos],
+  );
+
+  const deleteTodo = useCallback(
+    (id) => {
+      setTodos((current) => current.filter((todo) => todo.id !== id));
+    },
+    [setTodos],
+  );
+
+  return { todos, addTodo, toggleTodo, deleteTodo };
+}
